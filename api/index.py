93336e60d76f8ad40a036e4880aa27d24a94dd1c
tathya <tathyaorigins@gmail.com>
2024-09-01 22:51:27 +0530

    Initial Commit

--- api/index.py
+++ api/index.py
@@ -1,33 +1,21 @@
+from flask import Flask, jsonify
 import os
 import pickle
-import requests
-from flask import Flask, jsonify, abort
-from flask_cors import CORS
-from dotenv import load_dotenv
 
 app = Flask(__name__)
-CORS(app)  
-load_dotenv()
 
-
-# file_path = os.path.join(os.path.dirname(__file__), 'reduced_movies.pkl')
-# with open(file_path, 'rb') as f:
-#     reduced_data = pickle.load(f)
-
-
-@app.route("/")
-def recommend(movie):
-
-    return jsonify(["hello", "hi"])
+# Load the .pkl file relative to the current file's directory
+file_path = os.path.join(os.path.dirname(__file__), 'reduced_movies.pkl')
+with open(file_path, 'rb') as f:
+    reduced_data = pickle.load(f)
 
 @app.route("/recommend/<string:movie>")
-def recommend(movie):
-
-    return jsonify(["hello", "hi"])
+def home(movie):
+    return jsonify(reduced_data[0])
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return jsonify({"status": 404, "message": e.description}), 404
+    return jsonify({"status": 404, "message": "Not Found"}), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
