a995ea6feb40176a1ac7d21792d9a57f518f6085
tathya <tathyaorigins@gmail.com>
2024-09-01 22:28:42 +0530

    Initial Commit

--- api/index.py
+++ api/index.py
@@ -1,19 +1,21 @@
 from flask import Flask, jsonify
+import os
 import pickle
 
 app = Flask(__name__)
-reduced_data = pickle.load(open('reduced_movies.pkl', 'rb'))
-data = reduced_data.tolist()
+
+# Load the .pkl file relative to the current file's directory
+file_path = os.path.join(os.path.dirname(__file__), 'reduced_movies.pkl')
+with open(file_path, 'rb') as f:
+    reduced_data = pickle.load(f)
+
 @app.route("/")
 def home():
-    return jsonify(data[0])
-
+    return jsonify(reduced_data[0])
 
 @app.errorhandler(404)
 def page_not_found(e):
     return jsonify({"status": 404, "message": "Not Found"}), 404
-
-
 
 if __name__ == '__main__':
     app.run(debug=True)
