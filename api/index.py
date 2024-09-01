114eed5f3e3f5fc44e2647a48553c06c8fde439d
tathya <tathyaorigins@gmail.com>
2024-09-01 22:41:36 +0530

    Initial Commit

--- api/index.py
+++ api/index.py
@@ -23,39 +23,8 @@ file_path = os.path.join(os.path.dirname(__file__), 'reduced_movies.pkl')
 with open(file_path, 'rb') as f:
     reduced_data = pickle.load(f)
 
-def get_poster_path(movie_id, api_key):
-    try:
-        url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key={api_key}"
-        response = requests.get(url)
-        response.raise_for_status()  # Raise an exception for HTTP errors
-        data = response.json()
-        poster_path = data.get('poster_path')
-        if poster_path:
-            return f"https://image.tmdb.org/t/p/w500{poster_path}"
-        return None
-    except requests.exceptions.RequestException as e:
-        print(f"Error fetching poster for movie ID {movie_id}: {e}")
-        return None
-
 @app.route("/recommend/<string:movie>")
 def recommend(movie):
-    # movie_data = reduced_data[movie.title()]
-
-    # if not movie_data:
-    #     return abort(404, description="Movie not found")
-
-    # recommended_movies_posters = []
-    
-    # for recommended_movie in movie_data['recommendations']:
-    #     poster_url = get_poster_path(recommended_movie['movie_id'], TMDB_API_KEY)
-    #     if poster_url:
-    #         recommended_movies_posters.append({
-    #             "title": recommended_movie.get('title'),
-    #             "poster_url": poster_url
-    #         })
-    
-    # if not recommended_movies_posters:
-    #     return jsonify({"message": "No recommendations found"}), 404
 
     return jsonify(reduced_data[0])
 
