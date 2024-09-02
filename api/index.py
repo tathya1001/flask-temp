4302bb70939f92283dbf300d44c3c8331b5b564a
Tathya <121713727+tathya1001@users.noreply.github.com>
2024-09-02 23:48:11 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,8 +1,10 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 import os
 import pickle
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
 
 # Load the .pkl file relative to the current file's directory
 file_path = os.path.join(os.path.dirname(__file__), 'reduced_movies.pkl')
